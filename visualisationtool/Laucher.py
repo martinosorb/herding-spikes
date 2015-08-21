31fbbb5414c8b216ddc0c6f91bd0a97716814f56
cesar0205 <cesar_ipn2@hotmail.com>
2015-08-21 01:43:02 +0100

    Create Laucher.py

---
+++ visualisationtool/Laucher.py
@@ -0,0 +1,10 @@
+import sys
+from WindowDesign import *
+from MainWindow import MainWindow
+
+
+if __name__ == "__main__":
+	app =QtGui.QApplication(sys.argv)
+	myapp = MainWindow()
+	myapp.show()
+	sys.exit(app.exec_())
