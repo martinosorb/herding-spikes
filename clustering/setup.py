7d6ec530da99874542158e546801c6e6452089dc
Martino Sorbaro <martinosorb@users.noreply.github.com>
2015-09-04 11:32:45 +0200

    Update setup.py

--- clustering/setup.py
+++ clustering/setup.py
@@ -1,5 +1,26 @@
-from distutils.core import setup
-setup(name='herdingspikes',
-      version='0.2',
-      py_modules=['herdingspikes','ms_new_withmp'],
-      )
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+#def read(fname):
+#    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "herding-spikes",
+    version = "0.0.4",
+    author = "Martino Sorbaro",
+    author_email = "martino.sorbaro@ed.ac.uk",
+    description = "Software for high density multi-electrode array recordings -- clustering",
+    license = "GPL",
+    keywords = "spikes neuro sorting",
+    url = "https://github.com/martinosorb/herding-spikes",
+    py_modules=['herdingspikes','ms_new_withmp'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: GPL License",
+    ],
+)
